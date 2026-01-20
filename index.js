b5cb8f0fd3cfb047e990e4af98318aaf4b6904d4
Lalit Magar <lalitmagar1729@gmail.com>
2026-01-20 12:49:45 +0530

    Change server port from 3000 to 3001

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.use((req, res, next) => {
     res.status(404).render("404");
 });
 
-app.listen(3000, function() {
-    console.log('Server is running on http://localhost:3000');
+app.listen(3001, function() {
+    console.log('Server is running on http://localhost:3001');
 });
